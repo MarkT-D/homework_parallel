8872b964962a60aa2bd94efd2d5f35d53f38e5e4
Esther <esther.ottens@student.uva.nl>
2025-11-22 10:47:38 +0100

    forgot to add the sequential function to the header :(

--- lab_2/assign2_1/simulate.hh
+++ lab_2/assign2_1/simulate.hh
@@ -21,4 +21,19 @@
 double *simulate(const long i_max, const long t_max, const long block_size,
                  double *old_array, double *current_array, double *next_array);
 
+/*
+ * Executes the entire simulation.
+ *
+ * Implement your code here.
+ *
+ * i_max: how many data points are on a single wave
+ * t_max: how many iterations the simulation should run
+ * num_threads: how many threads to use (excluding the main threads)
+ * old_array: array of size i_max filled with data for t-1
+ * current_array: array of size i_max filled with data for t
+ * next_array: array of size i_max. You should fill this with t+1
+ */
+double *simulateSeq(const int i_max, const int t_max, const int num_threads,
+        double *old_array, double *current_array, double *next_array);
+
 #endif
