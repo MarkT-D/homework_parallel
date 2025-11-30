accacb91c95b8ec9e5d35be094eebfd6b3f08cc2
Esther <esther.ottens@student.uva.nl>
2025-11-30 18:25:57 +0100

    adding function to header

--- lab_3/simulate.h
+++ lab_3/simulate.h
@@ -4,5 +4,8 @@
 
 #pragma once
 
+int MYMPI_Bcast (void* buffer, int count , MPI_Datatype datatype, int root,
+        MPI_Comm communicator);
+
 double *simulate(const int i_max, const int t_max, double *old_array,
         double *current_array, double *next_array);
